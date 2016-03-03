bb91013eaa4cb56d048623646a34cee86a21b2ad
mniedre <webcane@ya.ru>
2016-03-03 16:07:40 +0300

    use annotated compontent config with aspect

---
+++ knights/src/main/java/com/springinaction/knights/KingdomTalesConfiguration.java
@@ -0,0 +1,13 @@
+package com.springinaction.knights;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+public class KingdomTalesConfiguration {
+
+}
+
--- knights/src/main/java/com/springinaction/knights/Minstrel.java
+++ knights/src/main/java/com/springinaction/knights/Minstrel.java
@@ -1,14 +1,25 @@
 package com.springinaction.knights;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component("minstrel")
 public class Minstrel {
 
+	@Pointcut("execution(void com.springinaction.knights.Knight.embarkOnQuest())")
+	public void embark() {
+	}
+
+	@Before("embark()")
 	public void singBeforeQuest() {
 		System.out.println("Fa la la; The knight is so brave!");
 	}
 
+	@After("embark()")
 	public void singAfterQuest() {
 		System.out.println("Tee hee he; The brave knight did embark on a quest!");
 	}
